359f7c9a5f10414437123e61e26b3d628977166a
Adwait Raste <adwait.raste@research.iiit.ac.in>
2020-02-03 13:50:27 +0530

    Initial Commit

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+document.write(`
+<footer>
+        <div class="footer-container">
+            <a href="https://github.com/adwaitraste" class="github" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+            <a href="https://www.instagram.com/4dw4it/" class="insta" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        </div>
+</footer>
+`);
---
+++ js/header.js
@@ -0,0 +1,19 @@
+document.write(`
+<header>
+    <div class="nav-container">
+        <nav>
+            <input type="checkbox" id="toggle">
+            <label for="toggle" class="burger-menu">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </label>
+            <div class="menu">
+                <a href="./index.html" id="home">Home</a>
+                <a href="./about.html" id="about">About Me</a>
+                <a href="./contact.html" id="contact">Contact Me</a>
+                <a href="#top"><i class="fa fa-chevron-circle-up" aria-hidden="true"></i></a>
+            </div>
+            <a href="./index.html" class="logo"><span>Adwait</span> Raste</a>
+        </nav>
+    </div>
+</header>
+`);
